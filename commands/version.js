e6bbfa97dd5054acdb87886fb3725dc760f387f0
Ben Duran <ben@bytebug.io>
2022-03-21 20:29:42 -0400

    added os info to ver command

--- commands/version.js
+++ commands/version.js
@@ -2,11 +2,17 @@ module.exports = {
   name: 'version',
   descrption: 'This a version command',
   execute(message, args) {
+    const { platform } = require('process');
     // const { admin_role } = require('../config.json');
     // if you want to validate with roles...
     // if (message.member.roles.cache.has(admin_role))
     //   message.channel.send('DarK`BoT by Shiva ver: 0.0.1');
     // else message.channel.send('You don't have the right permissions')
-    message.channel.send('DarK`BoT by Shiva ver: 0.0.1');
+    const os_info =
+      platform.charAt(0).toUpperCase() + platform.slice(1);
+
+    message.channel.send(
+      'DarK`BoT by Shiva ver: 0.0.1 Running on: ' + os_info
+    );
   },
 };
