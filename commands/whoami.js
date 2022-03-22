af15d7fefe685caafe5f3a0523096f670691a6ce
Ben Duran <ben@bytebug.io>
2022-03-22 11:06:55 -0400

    fixed permissions issue

--- commands/whoami.js
+++ commands/whoami.js
@@ -3,14 +3,13 @@ module.exports = {
   descrption: 'This a whoami commands',
   execute(message, args) {
     const { admin_role } = require('../config.json');
-    let id = '692963810193178664';
-    let isAdmin = message.guild.roles.cache.get(admin_role);
-    console.log(isAdmin);
 
-    isAdmin
-      ? message.channel.send(
-          `You are ${message.author.username} and you are a Admin`
-        )
-      : message.channel.send(`You are ${message.author.username}`);
+    if (message.member.roles.cache.has(admin_role)) {
+      message.channel.send(
+        `You are ${message.author.username} and you are an Admin.`
+      );
+    } else {
+      message.channel.send(`You are ${message.author.username}`);
+    }
   },
 };
