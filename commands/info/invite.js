d9c7a0d57b7cbdf9920efd55e23fcb4feecc9c99
Benjamin Duran <benjamind10@pm.me>
2022-03-27 09:01:02 -0400

    fixed some issues with bot stats

---
+++ commands/info/invite.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'invite',
+  description: "Get the bot's",
+  permissions: ['ADMINISTRATOR'],
+  async execute(client, message, args) {
+    let embed = new Discord.MessageEmbed()
+      .setTitle('Invite Me')
+      .setColor('RANDOM')
+      .setDescription(
+        "**Get DarK`BoT's Invite Link [Here](https://discord.com/api/oauth2/authorize?client_id=955227986318225458&permissions=8&scope=bot)**\n**"
+      )
+      .setFooter({ text: `Requested By: ${message.author.username}` });
+    message.channel.send({ embeds: [embed] });
+  },
+};
