45adee682ac2eae15a9427971c43e551e761a761
Benjamin Duran <benjamind10@pm.me>
2022-03-27 14:51:33 -0400

    added sys uptime

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -13,6 +13,22 @@ module.exports = {
   permissions: ['SEND_MESSAGES'],
   cooldown: 10,
   async execute(client, message, args, level) {
+    function systemUptime(seconds) {
+      seconds = os.uptime();
+      var d = Math.floor(seconds / (3600 * 24));
+      var h = Math.floor((seconds % (3600 * 24)) / 3600);
+      var m = Math.floor((seconds % 3600) / 60);
+      var s = Math.floor(seconds % 60);
+
+      var dDisplay = d > 0 ? d + (d == 1 ? ' day, ' : ' days, ') : '';
+      var hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : '';
+      var mDisplay = m > 0 ? m + (m == 1 ? ' minute, ' : ' minutes, ') : '';
+      var sDisplay = s > 0 ? s + (s == 1 ? ' second' : ' seconds') : '';
+      return dDisplay + hDisplay + mDisplay + sDisplay;
+    }
+
+    let sysUptime = systemUptime();
+
     let commands = client.commands.map(command => {
       return command.name;
     });
@@ -66,7 +82,9 @@ module.exports = {
           .addField(':book: Node Version', `${process.version}`, true)
           .addField(
             ':stopwatch: Uptime & Ping',
-            `${duration} / ${Math.round(client.ws.ping)}ms`,
+            `${duration} / ${Math.round(
+              client.ws.ping
+            )}ms | System: ${sysUptime}`,
             true
           );
         //.addField(":stopwatch: Server uptime", `${prettyMs(oss.sysUptime())}`, true)
