288911e0608e6d49e39e349c4d9d23c6bd837766
Ben <benjamind10@pm.me>
2022-07-14 15:18:15 -0400

    removed unecesary comment

--- commands/core/version.js
+++ commands/core/version.js
@@ -11,11 +11,7 @@ module.exports = {
       .toString()
       .trim()
       .slice(0, 7);
-    // const { admin_role } = require('../config.json');
-    // if you want to validate with roles...
-    // if (message.member.roles.cache.has(admin_role))
-    //   message.channel.send('DarK`BoT by Shiva ver: 0.0.1');
-    // else message.channel.send('You don't have the right permissions')
+
     const platform = os.platform();
     const version = os.release();
     const os_info = platform.charAt(0).toUpperCase() + platform.slice(1);
