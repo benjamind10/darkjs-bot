d5d12b4b2ef7cb2f4bfee3759740ae38affc1a1a
Benjamin <benjamind10@pm.me>
2022-11-09 22:08:37 -0500

    attempt fix

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -3,95 +3,91 @@ const cooldowns = new Map();
 const { Collection } = require('discord.js');
 
 module.exports = (Discord, client, message) => {
-  try {
-    const prefix = client.config.px;
+  const prefix = client.config.px;
 
-    const validPermissions = [
-      'CREATE_INSTANT_INVITE',
-      'KICK_MEMBERS',
-      'BAN_MEMBERS',
-      'ADMINISTRATOR',
-      'MANAGE_CHANNELS',
-      'MANAGE_GUILD',
-      'ADD_REACTIONS',
-      'VIEW_AUDIT_LOG',
-      'PRIORITY_SPEAKER',
-      'STREAM',
-      'VIEW_CHANNEL',
-      'SEND_MESSAGES',
-      'SEND_TTS_MESSAGES',
-      'MANAGE_MESSAGES',
-      'EMBED_LINKS',
-      'ATTACH_FILES',
-      'READ_MESSAGE_HISTORY',
-      'MENTION_EVERYONE',
-      'USE_EXTERNAL_EMOJIS',
-      'VIEW_GUILD_INSIGHTS',
-      'CONNECT',
-      'SPEAK',
-      'MUTE_MEMBERS',
-      'DEAFEN_MEMBERS',
-      'MOVE_MEMBERS',
-      'USE_VAD',
-      'CHANGE_NICKNAME',
-      'MANAGE_NICKNAMES',
-      'MANAGE_ROLES',
-      'MANAGE_WEBHOOKS',
-      'MANAGE_EMOJIS',
-    ];
+  const validPermissions = [
+    'CREATE_INSTANT_INVITE',
+    'KICK_MEMBERS',
+    'BAN_MEMBERS',
+    'ADMINISTRATOR',
+    'MANAGE_CHANNELS',
+    'MANAGE_GUILD',
+    'ADD_REACTIONS',
+    'VIEW_AUDIT_LOG',
+    'PRIORITY_SPEAKER',
+    'STREAM',
+    'VIEW_CHANNEL',
+    'SEND_MESSAGES',
+    'SEND_TTS_MESSAGES',
+    'MANAGE_MESSAGES',
+    'EMBED_LINKS',
+    'ATTACH_FILES',
+    'READ_MESSAGE_HISTORY',
+    'MENTION_EVERYONE',
+    'USE_EXTERNAL_EMOJIS',
+    'VIEW_GUILD_INSIGHTS',
+    'CONNECT',
+    'SPEAK',
+    'MUTE_MEMBERS',
+    'DEAFEN_MEMBERS',
+    'MOVE_MEMBERS',
+    'USE_VAD',
+    'CHANGE_NICKNAME',
+    'MANAGE_NICKNAMES',
+    'MANAGE_ROLES',
+    'MANAGE_WEBHOOKS',
+    'MANAGE_EMOJIS',
+  ];
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const cmd = args.shift().toLowerCase();
 
-    const command = client.commands.get(cmd);
+  const command = client.commands.get(cmd);
 
-    if (command === undefined) {
-      return message.reply('No command found');
-    }
-
-    if (command.permissions.length) {
-      let invalidPerms = [];
-      for (const perm of command.permissions) {
-        if (!validPermissions.includes(perm)) {
-          return console.log(`Invalid Permissions ${perm}`);
-        }
-        if (!message.member.permissions.has(perm)) {
-          invalidPerms.push(perm);
-        }
-      }
-      if (invalidPerms.length) {
-        return message.channel.send(`Missing Permissions: \`${invalidPerms}\``);
-      }
-    }
-    if (!cooldowns.has(command.name)) {
-      cooldowns.set(command.name, new Collection());
-    }
-
-    const current_time = Date.now();
-    const time_stamps = cooldowns.get(command.name);
-    const cooldown_amount = command.cooldown * 1000;
-
-    if (time_stamps.has(message.author.id)) {
-      const expiration_time =
-        time_stamps.get(message.author.id) + cooldown_amount;
-
-      if (current_time < expiration_time) {
-        const time_left = (expiration_time - current_time) / 1000;
-
-        return message.reply(
-          `Please wait ${time_left.toFixed(1)} more seconds before using ${
-            command.name
-          }`
-        );
-      }
-    }
-
-    time_stamps.set(message.author.id, current_time);
-  } catch (e) {
-    console.log(e);
+  if (command === undefined) {
+    return message.reply('No command found');
   }
+
+  if (command.permissions.length) {
+    let invalidPerms = [];
+    for (const perm of command.permissions) {
+      if (!validPermissions.includes(perm)) {
+        return console.log(`Invalid Permissions ${perm}`);
+      }
+      if (!message.member.permissions.has(perm)) {
+        invalidPerms.push(perm);
+      }
+    }
+    if (invalidPerms.length) {
+      return message.channel.send(`Missing Permissions: \`${invalidPerms}\``);
+    }
+  }
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Collection());
+  }
+
+  const current_time = Date.now();
+  const time_stamps = cooldowns.get(command.name);
+  const cooldown_amount = command.cooldown * 1000;
+
+  if (time_stamps.has(message.author.id)) {
+    const expiration_time =
+      time_stamps.get(message.author.id) + cooldown_amount;
+
+    if (current_time < expiration_time) {
+      const time_left = (expiration_time - current_time) / 1000;
+
+      return message.reply(
+        `Please wait ${time_left.toFixed(1)} more seconds before using ${
+          command.name
+        }`
+      );
+    }
+  }
+
+  time_stamps.set(message.author.id, current_time);
 
   try {
     command.execute(client, message, args, Discord);
